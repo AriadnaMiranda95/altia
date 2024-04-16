a46781f141f486f01e9a1715f2868a9c711f2624
Ariadna Miranda Santana <aryadna10001@gmail.com>
2024-04-16 20:30:41 +0100

    Feat: Add Spinner, no records message.

--- src/components/FilmsDisplayer.vue
+++ src/components/FilmsDisplayer.vue
@@ -2,8 +2,15 @@
   <div class="filmsDisplayerWrapper">
     <h1>Buscador pelis</h1>
     <searcher v-model="search" @search="() => getFilms(search)"/>
-    <div class="cardWrapper" >
-      <film-card v-for="film in films" :key="film" :title="film.Title" :subtitle="film.Year" :img="film.Poster" />
+      <loading v-if="loading" />
+    <div v-else class="cardWrapper"  >
+      <template v-if="existRecords">
+        <film-card  v-for="film in films" :key="film" :title="film.Title" :subtitle="film.Year" :img="film.Poster" />
+      </template>
+      <div class="noRecords" v-else>
+        <span>Ups! Parece que no se encontaron resultados...</span>
+        <span> :(</span>
+      </div>
     </div>
   </div>
 </template>
@@ -11,18 +18,20 @@
 <script setup>
 import { ref, computed } from 'vue'
 import { useStore } from 'vuex'
-import Searcher from './Searcher.vue';
+import Loading from '@/components/Loading.vue';
 import FilmCard from './FilmCard.vue';
+import Searcher from './Searcher.vue';
 const store = useStore()
 const search = ref()
 
+const loading = computed(() => store.getters[`FilmsStore/loading`])
 const films = computed(() => store.getters['FilmsStore/films'])
-
+const existRecords = computed(() => films.value.length > 0 )
 const getFilms = async (query) => {
   await store.dispatch('FilmsStore/getFilms', query)
 }
 
 
 
-getFilms('Superman')
+getFilms()
 </script>
--- src/store/filmsStore.js
+++ src/store/filmsStore.js
@@ -3,27 +3,41 @@ import API from '../services/api/films'
 export default {
   namespaced: true,
   actions: {
-    async getFilms({ commit }, payload) {
+    async getFilms({ commit }, payload) {      
       try {
+        commit('clearRecords')
+        commit('setLoading', true)
         const { data } = await API.getFilms(payload)
-        commit('setFilms', data.Search)
+        if (data.Search) commit('setFilms', data.Search)
       } catch (error) {
         console.log('error', error)
+      } finally {
+        commit('setLoading', false)
       }
-    }
+    },
   },
   state: {
-    films: []
+    films: [],
+    loading:  false,
   },
   getters: {
     films(state) {
-      console.log(state.films)
       return state.films
+    },
+    loading(state) {
+      return state.loading
     }
   },
+
   mutations: {
     setFilms(state, payload) {
       state.films = [...payload]
+    },
+    setLoading(state, payload) {
+      state.loading = payload
+    },
+    clearRecords(state) {
+      state.films = []
     }
   }
 }
